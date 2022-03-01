0f8fae5581d37339cfbbf90bb556976f4fe4e5a8
Ryan Dens <rdens@traceable.ai>
2022-03-01 15:20:53 -0800

    :recycle: async communicaiton

--- client/src/main/java/com/ryandens/grpc/Client.java
+++ client/src/main/java/com/ryandens/grpc/Client.java
@@ -2,20 +2,41 @@ package com.ryandens.grpc;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import java.util.concurrent.CountDownLatch;
 import org.hypertrace.example.GreeterGrpc;
-import org.hypertrace.example.GreeterGrpc.GreeterBlockingStub;
+import org.hypertrace.example.GreeterGrpc.GreeterStub;
 import org.hypertrace.example.Helloworld.Request;
 import org.hypertrace.example.Helloworld.Response;
 
 public final class Client {
 
-  public static void main(final String[] args) {
+  public static void main(final String[] args) throws InterruptedException {
     System.out.println("Starting client");
     ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 8080)
         .usePlaintext()
         .build();
-    GreeterBlockingStub greeterStub = GreeterGrpc.newBlockingStub(managedChannel);
-    Response response = greeterStub.sayHello(Request.newBuilder().setName("foo").build());
-    System.out.println(response.getMessage());
+    GreeterStub greeterStub = GreeterGrpc.newStub(managedChannel);
+    CountDownLatch countDownLatch = new CountDownLatch(1);
+    StreamObserver<Response> responseObserver = new StreamObserver<>() {
+      @Override
+      public void onNext(Response value) {
+        System.out.println(value.getMessage());
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        countDownLatch.countDown();
+        System.out.println(t.getMessage());
+      }
+
+      @Override
+      public void onCompleted() {
+        countDownLatch.countDown();
+        System.out.println("Completed");
+      }
+    };
+    greeterStub.sayHello(Request.newBuilder().setName("foo").build(), responseObserver);
+    countDownLatch.await();
   }
 }
